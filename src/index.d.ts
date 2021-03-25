69c92d492d49b40ed6a517958fa6f1d14e0adaa7
Dominic <dominic.tobias@gmail.com>
2021-03-26 01:10:14 +0800

    Make value type in resolve optional (fix type, non-functional)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,25 @@
 export interface Storage<K = IDBValidKey> {
-  get<T = any>(key: K): T | undefined | Promise<T | undefined>;
-  set<T = any>(key: K, value: T): void;
-  keys(): K[] | Promise<K[]>;
-  del(key: K): void;
-  clear(): void;
+  get<T = any>(key: K): T | undefined | Promise<T | undefined>
+  set<T = any>(key: K, value: T): void
+  keys(): K[] | Promise<K[]>
+  del(key: K): void
+  clear(): void
 }
 
-export type Duration = number | string;
+export type Duration = number | string
 
 export interface Options {
-  cleanupInterval?: Duration;
-  customStorage?: Storage;
+  cleanupInterval?: Duration
+  customStorage?: Storage
 }
 
 export interface Cache<K = IDBValidKey> {
-  resolve<T = any>(key: K, value: T | (() => T | Promise<T>), expiresIn?: Duration): T;
-  del(key: K, matchStart?: boolean): ReturnType<Storage['del']>;
-  clear(): ReturnType<Storage['clear']>;
-  teardown(): void;
+  resolve<T = any>(key: K, value?: T | (() => T | Promise<T>), expiresIn?: Duration): T
+  del(key: K, matchStart?: boolean): ReturnType<Storage['del']>
+  clear(): ReturnType<Storage['clear']>
+  teardown(): void
 }
 
-declare const createCache: (options: Options) => Cache;
+declare const createCache: (options: Options) => Cache
 
-export default createCache;
+export default createCache
