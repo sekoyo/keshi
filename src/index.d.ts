bf919af7f2cbd924e989762fe00067813cec9e92
Pascal Garber <pascal@artandcode.studio>
2021-03-25 09:08:19 +0000

    Add typescript type definition file for InMemoryStorage

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,30 +1,28 @@
-declare module "keshi" {
-    export interface Storage {
-      get<T = any>(key: IDBValidKey): T | undefined | Promise<T | undefined>;
-      set<T = any>(key: IDBValidKey, value: T): void;
-      keys(): IDBValidKey[] | Promise<IDBValidKey[]>;
-      del(key: IDBValidKey): void;
-      clear(): void;
-    }
-  
-    export interface Options {
-      cleanupInterval?: string;
-      customStorage?: Storage;
-    }
-  
-    export interface Cache {
-      resolve<T = any>(
-        key: string,
-        value: T | (() => Promise<T>),
-        expiresIn: number | string
-      ): T;
-      del(key: string, matchStart: boolean): ReturnType<Storage["del"]>;
-      clear(): ReturnType<Storage["clear"]>;
-      teardown(): void;
-    }
-  
-    const createCache: (options: Options) => Cache;
-  
-    export default createCache;
-  }
-  
+export interface Storage {
+  get<T = any>(key: IDBValidKey): T | undefined | Promise<T | undefined>;
+  set<T = any>(key: IDBValidKey, value: T): void;
+  keys(): IDBValidKey[] | Promise<IDBValidKey[]>;
+  del(key: IDBValidKey): void;
+  clear(): void;
+}
+
+export interface Options {
+  cleanupInterval?: string;
+  customStorage?: Storage;
+}
+
+export interface Cache {
+  resolve<T = any>(
+    key: string,
+    value: T | (() => Promise<T>),
+    expiresIn: number | string
+  ): T;
+  del(key: string, matchStart: boolean): ReturnType<Storage["del"]>;
+  clear(): ReturnType<Storage["clear"]>;
+  teardown(): void;
+}
+
+declare const createCache: (options: Options) => Cache;
+
+export default createCache;
+
